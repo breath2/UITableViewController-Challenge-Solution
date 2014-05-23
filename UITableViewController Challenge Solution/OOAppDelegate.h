c768ce14654313dd8c9576bec2cf83da9b83f6bc
breath2 <breath2@breath2mini.local>
2014-05-23 13:49:57 +0800

    Initial Commit

---
+++ UITableViewController Challenge Solution/OOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OOAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by breath2 on 14-5-23.
+//  Copyright (c) 2014年 breath2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/OOViewController.h
@@ -0,0 +1,13 @@
+//
+//  OOViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by breath2 on 14-5-23.
+//  Copyright (c) 2014年 breath2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OOViewController : UIViewController
+
+@end
